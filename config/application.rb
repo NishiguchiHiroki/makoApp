97173ee73c93a53ee328322df8af7fb3968f6e22
NishiguchiHiroki <nishiguchihiroki2@gmail.com>
2019-12-02 00:51:22 +0900

    menu_setting

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module MakoApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.initialize_on_precompile = false
+    Rails.application.config.assets.precompile += %w( *.eot *.woff *.ttf *.svg *.otf *.png *.jpg *.jpeg *.gif vendor.css vendor.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
