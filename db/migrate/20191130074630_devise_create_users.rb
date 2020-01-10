3441697afe536d5c16e1f40b38f9ded3ff0dee7b
NishiguchiHiroki <nishiguchihiroki2@gmail.com>
2020-01-11 01:32:52 +0900

    login + logout_function setting

--- db/migrate/20191130074630_devise_create_users.rb
+++ db/migrate/20191130074630_devise_create_users.rb
@@ -3,7 +3,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email,                    null: false, default: "", unique: true
+      t.string :email,                    null: false, default: ""
       t.string :encrypted_password,       null: false, default: ""
       t.string :last_name,                null: false
       t.string :first_name,               null: false
@@ -12,7 +12,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.integer :birthdate_year,          null: false
       t.integer :birthdate_month,         null: false
       t.integer :birthdate_day,           null: false
-      t.string :phone_number,            null: false, unique: true
+      t.string :phone_number,            null: false
       
 
       ## Recoverable
@@ -25,7 +25,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users, :email                
+    add_index :users, :reset_password_token
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -106,8 +106,8 @@ ActiveRecord::Schema.define(version: 2020_01_08_164800) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "addresses", "users"
