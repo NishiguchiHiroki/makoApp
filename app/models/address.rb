87cf58a8127b33362973bf18592b52a0a8d0274b
NishiguchiHiroki <nishiguchihiroki2@gmail.com>
2020-01-23 21:04:07 +0900

    address_relation-test

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
-  has_one :user
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :order,         dependent: :destroy
-  belongs_to  :address
+  has_one  :address
   
   def prepare_order
     order || create_order
